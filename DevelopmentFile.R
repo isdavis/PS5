753b57b2300aad001f7e6005e2b4c7e70a1727fe
Ian <isdavis@wustl.edu>
2018-03-06 04:26:27 -0600

    a package

--- DevelopmentFile.R
+++ DevelopmentFile.R
@@ -2,15 +2,20 @@
 ## Load libraries and set working directory
 library(devtools)
 library(roxygen2)
-setwd("C:/Users/isdav/Documents/GitHub/PS5") #This will need to be changed to match your directory
+setwd("C:/Users/Ian Davis/Documents/GitHub/PS5") #This will need to be changed to match your directory
 getwd()
+
+##package.skeleton(name="IntegrateIt", code_files = ) 
+
+##I am still trying to figure out how package.skeleton works for S4; it would have made this whole assignment much easier.
+
 ## This is run once when the package strcuture is first created
 
-## At this point put the *.R files: integrateIt, printSimp and printTrap, plus the class R files
+## At this point put the *.R files: integrateIt, printSimp and printTrap, plus the class R files (Trapezoid.R and Simpson.R)
 ##into the correct directories and edit the DESCRIPTION file
 
 ## This can be run many times as the code is updated
-current.code <- as.package("squaresPack")
+current.code <- as.package("IntegrateIt")
 load_all(current.code)
 document(current.code)
 
