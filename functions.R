e7c3ca98758e5a365f542807237ba87e4cd67d9b
Ian <isdavis@wustl.edu>
2018-03-06 02:18:57 -0600

    everything is fine
    I'm dumb; if given a linear function, both simpsons and trapezoid rule will be equal and accurate. xd

--- functions.R
+++ functions.R
@@ -10,6 +10,8 @@ validT<-function(object) {
   errors<-character()
   if(length(ab)!=2) {errors<-"Invalid range."}
   if(length(x)<2) {errors<-"At least 2 points required to construct a curve."}
+  else if((x[2]-x[1])!=x[length(x)]-x[length(x)-1]) {errors<-"X values must be evenly spaced."}
+  #Elementary test to see if the points are evenly spaced
   if(ab[1]!=x[1] | ab[length(ab)]!=x[length(x)]) {errors<-"X values must match range."}
   if(length(x)!=length(y)) {errors<-"This is not a function."} 
   if(length(errors)==0) return(TRUE) else return(errors)
@@ -83,6 +85,7 @@ setMethod(f="integrateIt",
               for(i in 1:length(y)) {
                 z<-z+2*y[i]+2*y[i]*((i-1) %% 2)
               }
+              print(z)
               result<-((ab[2]-ab[1])/length(x))/3*(z-y[1]-y[length(y)])
               print(result)
             }
@@ -90,19 +93,7 @@ setMethod(f="integrateIt",
           }
 )
 
-integrateIt(c(1,2,3,4,5), c(1,2,3,4,5), c(1,5), "Trap")
-integrateIt(c(1,2,3,4,5), c(1,2,3,4,5), c(1,5), "Simpson")
+integrateIt(c(1,2,3,4,5), c(1,2,3,2,1), c(1,5), "Trap")
+integrateIt(c(1,2,3,4,5), c(1,2,3,1,1), c(1,5), "Simpson")
 
-setGeneric(name="addSquares",
-           def=function(x, y, ...)
-           {standardGeneric("addSquares")}
-)
-
-setMethod(f="addSquares",
-          definition=function(x, y, ...){
-            return(new("Squares", square=(x^2 + y^2), x = x, y = y))
-          }
-)
-
-addSquares(3,3)
 
